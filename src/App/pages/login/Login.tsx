c9c9dc73a933a471af5c41ececac719622d43449
Matheus Andrade <matheusandrade009@gmail.com>
2022-10-03 10:20:58 -0300

    feat: useEffect Hook

--- src/App/pages/login/Login.tsx
+++ src/App/pages/login/Login.tsx
@@ -1,28 +1,46 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-
-
+import { useEffect, useState } from "react";
 
 export const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  
-  const handleSignIn = () => {
-    // console.log(email)
-    // console.log(password)
-    
-  };
+
+  useEffect(() => {
+    //executa domente quando o componente for carregado somente uma vez
+  }, []);
+
+  useEffect(() => {
+    console.log(email);
+  }, [email]); //a função será executada toda vez que o email e a senha for alterada
+
+  useEffect(() => {
+    console.log(password);
+  }, [password]);
+
+  const handleSignIn = () => {};
   return (
     <div>
-      <form action="">
-        <label>
-          <span>Email</span>
-          <input type="text" value={email} onChange={e => setEmail(e.target.value)}/>
-        </label>
-        <label>
-          <span>Password</span>
-          <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
-        </label>
+      <form>
+        <div>
+          <label>
+            <span>Email</span>
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            <span>Password</span>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+        </div>
 
         <button type="button" onClick={handleSignIn}>
           Sign in
