7ed64b88de5aa0d9ed174ef4f725cb422890c48c
Matheus Andrade <matheusandrade009@gmail.com>
2022-10-21 11:19:15 -0300

    feat: useCallback hook

--- src/App/pages/login/Login.tsx
+++ src/App/pages/login/Login.tsx
@@ -1,23 +1,26 @@
-import { useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 
 export const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const emailLength = useMemo(()=>{
+  const emailLength = useMemo(()=>{  //evita fazer calculos complexos várias vezes
     return email.length * 1000;
   }, [email.length]);
 
 
-  //executa somente quando o componente for carregado somente uma vez
-  useEffect(() => {
+  // //executa somente quando o componente for carregado somente uma vez
+  // useEffect(() => {
+  //   console.log(email);
+  // }, [email]); //a função será executada toda vez que o email e a senha for alterada
+
+  // useEffect(() => {
+  //   console.log(password);
+  // }, [password]);
+
+  const handleSignIn = useCallback(()=>{ //guarda a função em memoria
     console.log(email);
-  }, [email]); //a função será executada toda vez que o email e a senha for alterada
-
-  useEffect(() => {
     console.log(password);
-  }, [password]);
-
-  const handleSignIn = () => {};
+  }, [email, password]);
 
 
   return (
